4957320e8dade3b379ae148395bfe2fa7782abaf
Prajwal Shetty <shettyprajwal009@gmail.com>
2026-08-23 22:22:46 +0530

    Time: 67 ms (30%), Space: 20 MB (28.24%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.py
@@ -0,0 +1,34 @@
+class Solution:
+    def sumGame(self, num: str) -> bool:
+
+        def calculate(num: str) -> tuple[int, int, int, int]:
+            left, right = 0, 0
+            left_q, right_q = 0, 0
+            mid = len(num) // 2
+
+            for index, char in enumerate(num):
+                if index < mid:
+                    if char == '?': left_q += 1
+                    else: left += int(char)
+                else:
+                    if char == '?': right_q += 1
+                    else: right += int(char)
+
+            return left, right, left_q, right_q
+
+        
+        left, right, left_q, right_q = calculate(num)
+
+        diff = left - right
+
+        if (left_q + right_q) % 2 == 1: return True
+
+        return diff != 9 * (right_q - left_q) // 2
+            
+
+    
+        
+            
+
+        
+        
